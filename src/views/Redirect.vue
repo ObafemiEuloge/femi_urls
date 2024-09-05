90f28a4e426a22429e698a1afc0f80c54c11b0f3
ObafemiEuloge <eagnoun@gmail.com>
2024-09-05 08:00:40 +0200

    Site de génération de liens courts

--- src/views/Redirect.vue
+++ src/views/Redirect.vue
@@ -1,35 +1,35 @@
 <template>
   <div>
-    <p></p>
+    <p>Redirection en cours...</p>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import { db } from '../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 
 const route = useRoute();
+const router = useRouter();
 
-async function fetchAndRedirect() {
+onMounted(async () => {
   const shortId = route.params.shortId;
-  
+
   try {
     const docRef = doc(db, 'links', shortId);
     const docSnap = await getDoc(docRef);
-    
+
     if (docSnap.exists()) {
       const longUrl = docSnap.data().longUrl;
-      window.location.href = longUrl; // Rediriger vers l'URL longue
+      window.location.href = longUrl; // Redirection vers l'URL longue
     } else {
       console.error('Aucune URL trouvée pour cet ID');
-      // Afficher une erreur ou rediriger vers une page d'erreur
+      router.push('/'); // Redirection en cas d'ID invalide
     }
   } catch (error) {
     console.error('Erreur lors de la récupération de l\'URL:', error);
+    router.push('/'); // Redirection en cas d'erreur
   }
-}
-
-// Appeler la fonction pour rediriger
-fetchAndRedirect();
+});
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    // Configuration du serveur de développement
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'https://femiurls.netlify.app/',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
